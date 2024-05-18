481bdcce19d2a0a0d60567bc80677b2b5a5b88f5
Moritz Roth <moritz@mroth.io>
2024-05-18 23:16:57 +0200

    Bootstrap stright.el

--- init.el
+++ init.el
@@ -44,13 +44,31 @@
 (require 'package)
 
 ;; Set up use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
 
+;; (setq use-package-always-ensure t)
+
+;; (require 'use-package)
+
+;; bootstrap stright.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(require 'use-package-ensure)
 (setq use-package-always-ensure t)
-
-(require 'use-package)
 
 (use-package no-littering
   :demand t
